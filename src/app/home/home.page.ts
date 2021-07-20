e94d6479719d2c93ee0afea69195db7aae1b6e32
DevArthurBarboza <arthur.barboza0016@edu.unifil.br>
2021-07-20 13:22:31 -0300

    Improving project!

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,33 +13,38 @@ export class HomePage {
   valorMB : number;
   valorGB : number; 
   
+  valorSaidaB : number;
+  valorSaidaKB : number;
+  valorSaidaMB : number;
+  valorSaidaGB : number;
+
 
   calculaB(valor){
     this.valorB = valor
-    this.valorKB = this.valorB / 1024
-    this.valorMB = this.valorB / (1024 **2) 
-    this.valorGB = this.valorB / (1024 **3)
+    this.valorSaidaKB = this.valorB / 1024
+    this.valorSaidaMB = this.valorB / (1024 **2) 
+    this.valorSaidaGB = this.valorB / (1024 **3)
   }
 
   calculaKB(valor){
     this.valorKB = valor
-    this.valorB = this.valorKB * 1024
-    this.valorMB = this.valorKB / 1024 
-    this.valorGB = this.valorKB / (1024 **2)
+    this.valorSaidaB = this.valorKB * 1024
+    this.valorSaidaMB = this.valorKB / 1024 
+    this.valorSaidaGB = this.valorKB / (1024 **2)
   }
 
   calculaMB(valor){
     this.valorMB = valor
-    this.valorKB = this.valorMB * 1024
-    this.valorB = this.valorMB * (1024 **2) 
-    this.valorGB = this.valorMB / 1024
+    this.valorSaidaKB = this.valorMB * 1024
+    this.valorSaidaB = this.valorMB * (1024 **2) 
+    this.valorSaidaGB = this.valorMB / 1024
   }
 
   calculaGB(valor){
     this.valorGB = valor
-    this.valorKB = this.valorGB * (1024**2)
-    this.valorMB = this.valorGB * 1024  
-    this.valorB = this.valorGB * (1024 **3)
+    this.valorSaidaKB = this.valorGB * (1024**2)
+    this.valorSaidaMB = this.valorGB * 1024  
+    this.valorSaidaB = this.valorGB * (1024 **3)
   }
 
   constructor() {}

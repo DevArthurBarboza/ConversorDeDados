d5ea8172e745235a2e0c471a4aa6f6e58cb7a607
DevArthurBarboza <arthur.barboza0016@edu.unifil.br>
2021-07-19 23:02:33 -0300

    Conversor de Dados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as internal from 'stream';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  valorB : number;  
+  valorKB : number; 
+  valorMB : number;
+  valorGB : number; 
+  
+
+  calculaB(valor){
+    this.valorB = valor
+    this.valorKB = this.valorB / 1024
+    this.valorMB = this.valorB / (1024 **2) 
+    this.valorGB = this.valorB / (1024 **3)
+  }
+
+  calculaKB(valor){
+    this.valorKB = valor
+    this.valorB = this.valorKB * 1024
+    this.valorMB = this.valorKB / 1024 
+    this.valorGB = this.valorKB / (1024 **2)
+  }
+
+  calculaMB(valor){
+    this.valorMB = valor
+    this.valorKB = this.valorMB * 1024
+    this.valorB = this.valorMB * (1024 **2) 
+    this.valorGB = this.valorMB / 1024
+  }
+
+  calculaGB(valor){
+    this.valorGB = valor
+    this.valorKB = this.valorGB * (1024**2)
+    this.valorMB = this.valorGB * 1024  
+    this.valorB = this.valorGB * (1024 **3)
+  }
 
   constructor() {}
 
